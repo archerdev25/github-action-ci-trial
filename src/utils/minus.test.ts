7c91b1d181e8b8a7a9fb036637288c75a6052b97
ORING\T25065 <econarcher2010@gmail.com>
2026-01-02 17:54:19 +0800

    QQQ

--- src/utils/minus.test.ts
+++ src/utils/minus.test.ts
@@ -6,4 +6,9 @@ describe("Failing sample test", () => {
    it('should return correct result', () => {
           expect(minus(1, 2)).toBe(-10);
       });
+
+     it('correct case', () => {
+          expect(minus(1, 6)).toBe(-5);
+      });
+
 });
