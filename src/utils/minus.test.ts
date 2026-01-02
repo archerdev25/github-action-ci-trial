18cc5dabb1ee912406b2fce5d4abb1a78a86d31a
ORING\T25065 <econarcher2010@gmail.com>
2026-01-02 17:12:43 +0800

    test: add an unit test that will fail

---
+++ src/utils/minus.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest";
+import { minus } from './minus';
+
+
+describe("Failing sample test", () => {
+   it('should return correct result', () => {
+          expect(minus(1, 2)).toBe(-10);
+      });
+});
